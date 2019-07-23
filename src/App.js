b79a05278e0b3eeac2477413070c1fa569ff69cc
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-07-23 07:01:46 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { Component } from "react";
+
+class App extends Component {
+    
+      
+      state = {
+        listOfPeople: []
+      };
+    
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    listOfPeople: data.people
+                })
+            })
+    };
+        render(){
+            return (
+                <div>
+                    {this.state.listOfPeople.map(person => person.name)}
+                </div>
+            )
+        };
+    }
+
+    export default App
